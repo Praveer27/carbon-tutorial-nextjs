29d277414ba20e7732570580072eb02f910d8358
Praveer Shetty <praveershetty@Praveers-MacBook-Pro.local>
2026-03-31 14:58:22 +0530

    feat(tutorial): complete step 1

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.extensionAlias = {
+      '.js': ['.ts', '.tsx', '.js', '.jsx'],
+    };
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
